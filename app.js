a790a8077c3d1e9d7f8257f662e4beba5016a799
Kat Sauma <katsauma@gmail.com>
2021-01-14 14:08:05 -0800

    countsAsAYes function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { countsAsAYes } from './utils.js';
 const quizButton = document.getElementById('launch-quiz');
 const resultsArea = document.getElementById('secret-div');
 
+
 // initialize state
 
 // set event listeners to update state and DOM
@@ -16,22 +17,21 @@ quizButton.addEventListener('click', () => {
 
     let correctAnswers = 0;
 
+
     const firstName = prompt('What is your first name?');
     const lastName = prompt('What is your last name?');
-    const firstAnswer = prompt('Did I live in a city outside the U.S.?');
 
+
+    const firstAnswer = prompt('Did I live in a city outside the U.S.?');
     if (countsAsAYes(firstAnswer)) correctAnswers++;
 
     const secondAnswer = prompt('Is music a particular passion I want to enmesh with coding?');
-
     if (!countsAsAYes(secondAnswer)) correctAnswers++;
 
     const thirdAnswer = prompt('Has my dance work used X-Box Kinect and Motion Capture before?');
-
     if (countsAsAYes(thirdAnswer)) correctAnswers++;
 
     const resultsString = `Wow, ${firstName} ${lastName}... you either know me really well, or not at all, but you got ${correctAnswers} on point. So you tell me!`;
-
     resultsArea.textContent = resultsString;
 
 });
